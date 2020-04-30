463df0eab8929d01025782e2e2137f8f53fabe8c
Valniria Bandeira <valniria.bandeira@to-brasil.com>
2020-04-30 09:19:42 -0300

    tipagem de variaveis em typescript

--- alurabank/app/ts/app.ts
+++ alurabank/app/ts/app.ts
@@ -1,1 +1,5 @@
-const negociacao = new Negociacao(new Date, 1, 100);
+const controller = new NegociacaoController();
+
+document
+.querySelector('.form')
+.addEventListener('submit', controller.adiciona.bind(controller));
